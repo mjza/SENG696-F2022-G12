4c5b51a53ac5ab624e5d811fc5b19387fc95e607
Mahdi J.Ansari <mahdi.jaberzadehansa@ucalgary.ca>
2022-11-29 15:11:01 -0700

    2 tables for DB designed

--- Assignment3/src/ca/ucalagary/seng696/g12/databases/InitializeDBTables.java
+++ Assignment3/src/ca/ucalagary/seng696/g12/databases/InitializeDBTables.java
@@ -14,23 +14,24 @@ import org.apache.derby.jdbc.*;
 public class InitializeDBTables {
 	public static void main(String[] args) {
 		InitializeDBTables e = new InitializeDBTables();
-		e.initDB();
-		e.readDB();
+		e.initUsersTable();
+		e.initResumesTable();
+		e.readUsers();
+		e.readResumes();
 	}
 
-	public void initDB() {
+	public void initUsersTable() {
 		Connection conn = null;
 		PreparedStatement pstmt;
 		Statement stmt;
-		ResultSet rs = null;
 		String createUsersSQL = "CREATE TABLE USERS ("
 				+ "ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"
 				+ "NAME VARCHAR(30) NOT NULL," 
 				+ "EMAIL VARCHAR(30)," 
 				+ "PASSWORD VARCHAR(30),"
 				+ "TYPE VARCHAR(1) CONSTRAINT TYPE_CONSTRAINT CHECK (TYPE IN ('P', 'C')),"
-				+ "CONSTRAINT PRIMARY_KEY PRIMARY KEY (ID))";
-
+				+ "CONSTRAINT USERS_PK PRIMARY KEY (ID))";
+		// Creating the table and insert initial data 
 		try {
 			Driver derbyEmbeddedDriver = new EmbeddedDriver();
 			DriverManager.registerDriver(derbyEmbeddedDriver);
@@ -77,16 +78,13 @@ public class InitializeDBTables {
 		    }
 
 		} catch (SQLException ex) {
-			if ("X0Y32".equals(ex.getSQLState()) == false)
 				System.out.println("in connection" + ex);
 		}
-		
+		// Closing the connection
 		try {
 			DriverManager.getConnection("jdbc:derby:;shutdown=true");
 		} catch (SQLException ex) {
-			if (ex.getErrorCode() == 50000) {
-				System.out.println("Derby shut down normally");
-			} else {
+			if (ex.getErrorCode() != 50000) {
 				System.err.println("Derby did not shut down normally");
 				System.err.println(ex.getMessage());
 			}
@@ -95,7 +93,64 @@ public class InitializeDBTables {
 		
 	}
 	
-	public void readDB() {
+	public void initResumesTable() {
+		Connection conn = null;
+		PreparedStatement pstmt;
+		Statement stmt;
+		String createResumesSQL = "CREATE TABLE RESUMES ("
+				+ "ID INTEGER NOT NULL,"
+				+ "KEYWORDS VARCHAR(100) NOT NULL," 
+				+ "BODY VARCHAR(1000)," 
+				+ "UPDATED_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+				+ "CONSTRAINT RESUMES_PK PRIMARY KEY (ID),"
+				+ "CONSTRAINT RESUMES_FK FOREIGN KEY (ID) REFERENCES USERS(ID))";
+		// Creating the table and insert initial data 
+		try {
+			Driver derbyEmbeddedDriver = new EmbeddedDriver();
+			DriverManager.registerDriver(derbyEmbeddedDriver);
+			conn = DriverManager.getConnection("jdbc:derby:DB;create=true", "SENG696", null);
+			conn.setAutoCommit(false);
+			stmt = conn.createStatement();
+			
+			DatabaseMetaData databaseMetadata = conn.getMetaData();
+		    ResultSet resultSet = databaseMetadata.getTables(null, null, "RESUMES", null);
+		    if (resultSet.next()) {
+		       System.out.println("TABLE RESUMES ALREADY EXISTS");
+		    } else {			
+				stmt.execute(createResumesSQL);
+	
+				pstmt = conn.prepareStatement("INSERT INTO RESUMES (ID,KEYWORDS,BODY) VALUES(?,?,?)");
+				pstmt.setString(1, "1");
+				pstmt.setString(2, "C++, JAVA, C#, SQLSERVER");
+				pstmt.setString(3, "I did 200 projects ...");
+				pstmt.executeUpdate();
+				
+				pstmt = conn.prepareStatement("INSERT INTO RESUMES (ID,KEYWORDS,BODY) VALUES(?,?,?)");
+				pstmt.setString(1, "2");
+				pstmt.setString(2, "PHP, MYSQL, PHYTHON");
+				pstmt.setString(3, "I am better than others ...");
+				pstmt.executeUpdate();
+				
+				conn.commit();
+		    }
+
+		} catch (SQLException ex) {
+				System.out.println("in connection" + ex);
+		}
+		// Closing the connection
+		try {
+			DriverManager.getConnection("jdbc:derby:;shutdown=true");
+		} catch (SQLException ex) {
+			if (ex.getErrorCode() != 50000) {
+				System.err.println("Derby did not shut down normally");
+				System.err.println(ex.getMessage());
+			}
+		}
+
+		
+	}
+	
+	public void readUsers() {
 		Connection conn = null;
 		Statement stmt;
 		ResultSet rs = null;
@@ -121,9 +176,40 @@ public class InitializeDBTables {
 		try {
 			DriverManager.getConnection("jdbc:derby:;shutdown=true");
 		} catch (SQLException ex) {
-			if (((ex.getErrorCode() == 50000) && ("XJ015".equals(ex.getSQLState())))) {
-				System.out.println("Derby shut down normally");
-			} else {
+			if (ex.getErrorCode() != 50000) {
+				System.err.println("Derby did not shut down normally");
+				System.err.println(ex.getMessage());
+			}
+		}
+	}
+	
+	public void readResumes() {
+		Connection conn = null;
+		Statement stmt;
+		ResultSet rs = null;
+		String readResumesSQL = "SELECT * FROM RESUMES";		
+
+		try {
+			Driver derbyEmbeddedDriver = new EmbeddedDriver();
+			DriverManager.registerDriver(derbyEmbeddedDriver);
+			conn = DriverManager.getConnection("jdbc:derby:DB;create=true", "SENG696", null);
+			conn.setAutoCommit(true);
+			stmt = conn.createStatement();
+
+			rs = stmt.executeQuery(readResumesSQL);
+			while (rs.next()) {
+				System.out.printf("%d, %s, %s, %s\n", rs.getInt(1), rs.getString(2), rs.getString(3),
+						rs.getString(4));
+			}
+
+		} catch (SQLException ex) {
+			System.out.println("in connection" + ex);
+		}
+
+		try {
+			DriverManager.getConnection("jdbc:derby:;shutdown=true");
+		} catch (SQLException ex) {
+			if (ex.getErrorCode() != 50000) {
 				System.err.println("Derby did not shut down normally");
 				System.err.println(ex.getMessage());
 			}
